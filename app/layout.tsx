d32974097e9fca1eaa0b9bf029ba9cb95acdf02f
Michael Uloth <hello@michaeluloth.com>
2025-11-05 21:12:07 -0500

    styles: move to root folder

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
-import './globals.css'
+import '@/styles/globals.css'
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
