5e05a030e35d9e24f59c80b75dcd6b09e9065f06
Michael Uloth <hello@michaeluloth.com>
2025-11-05 21:59:41 -0500

    notion: reduce line breaks

--- lib/notion/getPosts.ts
+++ lib/notion/getPosts.ts
@@ -2,61 +2,23 @@ import getDatabase from './getDatabase'
 
 export default async function getPosts(): Promise<any[]> {
   const posts = await getDatabase({
-    databaseId: process.env.NOTION_DB_ID_WRITING,
+    databaseId: process.env.NOTION_DB_ID_WRITING ?? '',
     filter: {
       and: [
-        {
-          property: 'Destination',
-          multi_select: {
-            contains: 'blog',
-          },
-        },
-        {
-          property: 'Status',
-          status: {
-            equals: 'Published',
-          },
-        },
-        {
-          property: 'Title',
-          title: {
-            is_not_empty: true,
-          },
-        },
-        {
-          property: 'Slug',
-          rich_text: {
-            is_not_empty: true,
-          },
-        },
-        {
-          property: 'Description',
-          rich_text: {
-            is_not_empty: true,
-          },
-        },
+        { property: 'Destination', multi_select: { contains: 'blog' } },
+        { property: 'Status', status: { equals: 'Published' } },
+        { property: 'Title', title: { is_not_empty: true } },
+        { property: 'Slug', rich_text: { is_not_empty: true } },
+        { property: 'Description', rich_text: { is_not_empty: true } },
         // NOTE: link posts don't have featured images atm
-        // {
-        //   property: 'Featured image',
-        //   url: {
-        //     is_not_empty: true,
-        //   },
-        // },
-        {
-          property: 'First published',
-          date: {
-            on_or_before: new Date().toISOString(),
-          },
-        },
+        // { property: 'Featured image', url: { is_not_empty: true } },
+        { property: 'First published', date: { on_or_before: new Date().toISOString() } },
       ],
     },
-    sorts: [
-      {
-        property: 'First published',
-        direction: 'descending',
-      },
-    ],
+    sorts: [{ property: 'First published', direction: 'descending' }],
   })
+
+  // TODO: parse with zod
 
   return posts
 }
