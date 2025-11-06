8ec81a732d99ceac4b6741e111370fb83f97683f
Michael Uloth <hello@michaeluloth.com>
2025-11-05 20:52:37 -0500

    next: enable turbopack file system cache for dev

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   cacheComponents: true, // see: https://nextjs.org/blog/next-16#cache-components
+  experimental: {
+    turbopackFileSystemCacheForDev: true, // see: https://nextjs.org/blog/next-16#turbopack-file-system-caching-beta
+  },
 }
 
 export default nextConfig
