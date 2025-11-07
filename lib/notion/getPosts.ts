003b4aac23436a1a18d8ba4e281853d83967dc42
Michael Uloth <hello@michaeluloth.com>
2025-11-06 23:44:26 -0500

    notion: remove unused code

--- lib/notion/getPosts.ts
+++ lib/notion/getPosts.ts
@@ -1,15 +1,12 @@
 import notion, { collectPaginatedAPI } from './client'
-import { getDataSource2 } from './getDataSource'
 
 /**
  * Fetches published blog posts from a Notion data source.
  * Filters posts by destination, status, title, slug, description, and publication date.
  * Sorts the posts by the first published date in descending order.
  *
- * @returns {Promise<any[]>} A promise that resolves to an array of published blog posts.
  * @see https://github.com/makenotion/notion-sdk-js?tab=readme-ov-file#collectpaginatedapilistfn-firstpageargs
  */
-
 export default async function getPosts(): Promise<any[]> {
   const posts = await collectPaginatedAPI(notion.dataSources.query, {
     data_source_id: process.env.NOTION_DATA_SOURCE_ID_WRITING ?? '',
@@ -27,23 +24,6 @@ export default async function getPosts(): Promise<any[]> {
     },
     sorts: [{ property: 'First published', direction: 'descending' }],
   })
-
-  // const posts = await getDataSource2({
-  // dataSourceId: process.env.NOTION_DATA_SOURCE_ID_WRITING ?? '',
-  // filter: {
-  //   and: [
-  //     { property: 'Destination', multi_select: { contains: 'blog' } },
-  //     { property: 'Status', status: { equals: 'Published' } },
-  //     { property: 'Title', title: { is_not_empty: true } },
-  //     { property: 'Slug', rich_text: { is_not_empty: true } },
-  //     { property: 'Description', rich_text: { is_not_empty: true } },
-  //     // NOTE: link posts don't have featured images atm
-  //     // { property: 'Featured image', url: { is_not_empty: true } },
-  //     { property: 'First published', date: { on_or_before: new Date().toISOString() } },
-  //   ],
-  // },
-  // sorts: [{ property: 'First published', direction: 'descending' }],
-  // })
 
   // TODO: parse with zod
 
