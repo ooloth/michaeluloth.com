662f79cf7f9b788a4f430cf4476797f9b89ee38a
Michael Uloth <hello@michaeluloth.com>
2025-11-06 23:54:53 -0500

    notion: delete more unused code

--- lib/notion/constants.ts
+++ lib/notion/constants.ts
@@ -3,6 +3,4 @@
 export const albumsDbId = process.env.NOTION_DB_ID_ALBUMS
 export const booksDbId = process.env.NOTION_DB_ID_BOOKS
 export const podcastsDbId = process.env.NOTION_DB_ID_PODCASTS
-export const postsDbId = process.env.NOTION_DB_ID_POSTS
-export const researchDbId = process.env.NOTION_DB_ID_RESEARCH
 export const writingDataSourceId = process.env.NOTION_DATA_SOURCE_ID_WRITING
