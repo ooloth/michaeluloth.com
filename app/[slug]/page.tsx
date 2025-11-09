0a058031a3f06c5224f423800476836dae185c4b
Michael Uloth <hello@michaeluloth.com>
2025-11-09 14:01:40 -0500

    post: migrate wip seo metadata from ui component to page.tsx

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -1,5 +1,3 @@
-// TODO: metadata: https://nextjs.org/docs/app/api-reference/functions/generate-metadata
-
 import getPost from '@/lib/notion/getPost'
 import getPosts from '@/lib/notion/getPosts'
 import getPropertyValue from '@/lib/notion/getPropertyValue'
@@ -21,8 +19,68 @@ export default async function DynamicRoute({ params }: Props) {
   // TODO: use fetch instead? https://nextjs.org/docs/app/api-reference/functions/fetch
   const post = await getPost({ slug, includeBlocks: true })
 
+  // TODO: metadata: https://nextjs.org/docs/app/api-reference/functions/generate-metadata
+  // const type = getPropertyValue(post.properties, 'Type')
+  // const title = getPropertyValue(post.properties, 'Title')
+  // const description = getPropertyValue(post.properties, 'Description')
+  // const featuredImage = getPropertyValue(post.properties, 'Featured image')
+  // const date = getPropertyValue(post.properties, 'First published')
+
+  // <ArticleSeo title={title} slug={slug} description={description} featuredImage={featuredImage} date={date} />
+
   return <Post post={post} />
 }
+
+// const ArticleSeo = ({ title, slug, description, featuredImage, date }) => {
+//   const url = `https://michaeluloth.com/${slug}`
+//   const formattedDate = new Date(date).toISOString()
+//
+//   const image = featuredImage
+//     ? featuredImage.includes('cloudinary')
+//       ? {
+//           url: transformCloudinaryImage(featuredImage, 1280),
+//           alt: title,
+//         }
+//       : {
+//           url: `https://michaeluloth.com${featuredImage}`,
+//           alt: title,
+//         }
+//     : {
+//         alt: 'Michael Uloth smiling into the camera',
+//         url: transformCloudinaryImage('https://res.cloudinary.com/ooloth/image/upload/mu/michael-landscape.jpg', 1280),
+//       }
+//
+//   return (
+//     <>
+//       <NextSeo
+//         title={title}
+//         description={description}
+//         canonical={url}
+//         openGraph={{
+//           type: 'article',
+//           article: {
+//             publishedTime: formattedDate,
+//           },
+//           url,
+//           title,
+//           description,
+//           images: [image],
+//         }}
+//       />
+//       <ArticleJsonLd
+//         authorName="Michael Uloth"
+//         dateModified={date}
+//         datePublished={date}
+//         description={description}
+//         images={[image.url]}
+//         publisherLogo="/static/favicons/android-chrome-192x192.png"
+//         publisherName="Michael Uloth"
+//         title={title}
+//         url={url}
+//       />
+//     </>
+//   )
+// }
 
 /**
  * Generates the list of static params (slugs) for all blog posts.
--- app/[slug]/ui/post.tsx
+++ app/[slug]/ui/post.tsx
@@ -1,4 +1,3 @@
-// import { NextSeo, ArticleJsonLd } from 'next-seo'
 // import { format } from 'timeago.js'
 
 import Blocks from '@/lib/notion/ui/Blocks'
@@ -8,21 +7,13 @@ import Heading from '@/ui/heading'
 // import Outer from '@/layouts/outer'
 // import Emoji from '@/ui/emoji'
 // import { transformCloudinaryImage } from '@/lib/cloudinary/utils'
-// import { useEffect } from 'react'
 
 export default function Post({ post }) {
-  const type = getPropertyValue(post.properties, 'Type')
   const title = getPropertyValue(post.properties, 'Title')
-  const slug = getPropertyValue(post.properties, 'Slug')
-  const description = getPropertyValue(post.properties, 'Description')
-  const featuredImage = getPropertyValue(post.properties, 'Featured image')
-  const date = getPropertyValue(post.properties, 'First published')
 
   return (
     <>
-      {/* <ArticleSeo title={title} slug={slug} description={description} featuredImage={featuredImage} date={date} /> */}
-
-      <article>
+      <article className="flex flex-col gap-y-4">
         <header>
           <Heading level={1}>{title}</Heading>
           {/* <p className="mt-3 text-sm text-gray-700 dark:text-gray-500">Updated {format(date)}</p> */}
@@ -37,61 +28,6 @@ export default function Post({ post }) {
     </>
   )
 }
-
-// const ArticleSeo = ({ title, slug, description, featuredImage, date }) => {
-//   const url = `https://michaeluloth.com/${slug}`
-//   const formattedDate = new Date(date).toISOString()
-//
-//   const image = featuredImage
-//     ? featuredImage.includes('cloudinary')
-//       ? {
-//           url: transformCloudinaryImage(featuredImage, 1280),
-//           alt: title,
-//         }
-//       : {
-//           url: `https://michaeluloth.com${featuredImage}`,
-//           alt: title,
-//         }
-//     : {
-//         alt: 'Michael Uloth smiling into the camera',
-//         url: transformCloudinaryImage('https://res.cloudinary.com/ooloth/image/upload/mu/michael-landscape.jpg', 1280),
-//       }
-//
-//   useEffect(() => {
-//     Prism.highlightAll()
-//   }, [])
-//
-//   return (
-//     <>
-//       <NextSeo
-//         title={title}
-//         description={description}
-//         canonical={url}
-//         openGraph={{
-//           type: 'article',
-//           article: {
-//             publishedTime: formattedDate,
-//           },
-//           url,
-//           title,
-//           description,
-//           images: [image],
-//         }}
-//       />
-//       <ArticleJsonLd
-//         authorName="Michael Uloth"
-//         dateModified={date}
-//         datePublished={date}
-//         description={description}
-//         images={[image.url]}
-//         publisherLogo="/static/favicons/android-chrome-192x192.png"
-//         publisherName="Michael Uloth"
-//         title={title}
-//         url={url}
-//       />
-//     </>
-//   )
-// }
 
 // const discussUrl = slug =>
 //   `https://mobile.twitter.com/search?q=${encodeURIComponent(
