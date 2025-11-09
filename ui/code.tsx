45b7f29cd87f59b633cdaec3e9bb6713fe6114c2
Michael Uloth <hello@michaeluloth.com>
2025-11-09 13:52:49 -0500

    code: remove copy button transformer from code blocks

--- ui/code.tsx
+++ ui/code.tsx
@@ -1,5 +1,3 @@
-// TODO: preferred: https://rehype-pretty.pages.dev/#react-server-component
-// TODO: backup: https://shiki.style/guide/install
 // TODO: backup: https://shiki.style/packages/next#react-server-component
 
 import { unified } from 'unified'
@@ -7,12 +5,16 @@ import remarkParse from 'remark-parse'
 import remarkRehype from 'remark-rehype'
 import rehypeStringify from 'rehype-stringify'
 import rehypePrettyCode from 'rehype-pretty-code'
-import { transformerCopyButton } from '@rehype-pretty/transformers'
 
 type Props = Readonly<{
   code: string
 }>
 
+/**
+ * Renders syntax-highlighted code from a markdown code block.
+ *
+ * @see https://rehype-pretty.pages.dev/#react-server-component
+ */
 export async function Code({ code }: Props) {
   const highlightedCode = await highlightCode(code)
 
@@ -24,14 +26,14 @@ async function highlightCode(code: string) {
     .use(remarkParse)
     .use(remarkRehype)
     .use(rehypePrettyCode, {
-      defaultLang: 'plaintext',
-      keepBackground: false,
-      transformers: [
-        transformerCopyButton({
-          visibility: 'always',
-          feedbackDuration: 3_000, // milliseconds
-        }),
-      ],
+      defaultLang: 'plaintext', // if no lang is specified
+      keepBackground: false, // set to false to apply a custom bg color
+      // See: https://rehype-pretty.pages.dev/#theme
+      // See: https://shiki.style/themes#themes
+      // theme: 'catppuccin-mocha',
+      theme: 'kanagawa-dragon',
+      // theme: 'nord',
+      // ],
     })
     .use(rehypeStringify)
     .process(code)
