a02c47681e935a66f2b4a7ebb24ec5394fab219d
Michael Uloth <hello@michaeluloth.com>
2025-11-10 01:25:54 -0500

    image: accept style overrides for image and wrapper elements

--- ui/image.tsx
+++ ui/image.tsx
@@ -2,8 +2,8 @@
 
 import transformCloudinaryImage from '@/lib/cloudinary/transformCloudinaryImage'
 
-const outerStyles = 'mt-6'
-const imageStyles = 'shadow-xl rounded bg-zinc-800'
+const outerStylesDefault = 'my-6'
+const imageStylesDefault = 'shadow-xl rounded bg-zinc-800'
 
 type Props = Readonly<{
   alt: string
@@ -11,20 +11,25 @@ type Props = Readonly<{
   height: number
   src: string
   width: number
+  imageStyles?: string
+  outerStyles?: string
 }>
 
 // TODO: handle Cloudinary and non-Cloudinary images differently
-export default function Image({ alt, caption, height, src, width }: Props) {
+export default function Image({ alt, caption, height, src, width, imageStyles, outerStyles }: Props) {
+  const imageClasses = imageStyles ? `${imageStylesDefault} ${imageStyles}` : imageStylesDefault
+  const outerClasses = outerStyles ? `${outerStylesDefault} ${outerStyles}` : outerStylesDefault
+
   const image = (
-    <img src={transformCloudinaryImage(src, 624)} alt={alt} width={width} height={height} className={imageStyles} />
+    <img src={transformCloudinaryImage(src, 624)} alt={alt} width={width} height={height} className={imageClasses} />
   )
 
   return caption ? (
-    <figure className={outerStyles}>
+    <figure className={outerClasses}>
       {image}
       <figcaption>{caption}</figcaption>
     </figure>
   ) : (
-    <div className={outerStyles}>{image}</div>
+    <div className={outerClasses}>{image}</div>
   )
 }
