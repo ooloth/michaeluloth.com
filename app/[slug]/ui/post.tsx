d457a734a77021107fa3de1934e40cd319307e6c
Michael Uloth <hello@michaeluloth.com>
2025-11-10 01:25:07 -0500

    post: add published and updated dates

--- app/[slug]/ui/post.tsx
+++ app/[slug]/ui/post.tsx
@@ -1,43 +1,49 @@
-// import { format } from 'timeago.js'
+import { format } from 'timeago.js'
 
 import NotionBlocks from '@/lib/notion/ui/NotionBlocks'
 import getPropertyValue from '@/lib/notion/getPropertyValue'
 import { Code } from '@/ui/code'
 import Heading from '@/ui/heading'
+import Paragraph from '@/ui/paragraph'
 
 export default function Post({ post }) {
   const title = getPropertyValue(post.properties, 'Title')
+  const datePublished = getPropertyValue(post.properties, 'First published')
 
   return (
     <article>
-      <Header title={title} />
+      <Header title={title} date={post.last_edited_time} />
       <NotionBlocks blocks={post.blocks} />
-      <Footer post={post} />
+      <Footer post={post} date={datePublished} />
     </article>
   )
 }
 
 type HeaderProps = Readonly<{
   title: string
+  date: string
 }>
 
-function Header({ title }: HeaderProps) {
+function Header({ title, date }: HeaderProps) {
   return (
-    <header className="mb-8">
+    <header className="mb-6">
       <Heading level={1}>{title}</Heading>
-      {/* <Paragraph className="mt-3 text-sm text-gray-700 dark:text-gray-500">Updated {format(date)}</p> */}
+      <Paragraph className="mt-1 italic text-zinc-400">Last updated {format(date)}</Paragraph>
     </header>
   )
 }
 
 type FooterProps = Readonly<{
   post: any
+  date: string
 }>
 
-function Footer({ post }: FooterProps) {
+function Footer({ post, date }: FooterProps) {
   return (
-    <footer className="my-12">
-      <details>
+    <footer className="my-10">
+      <Paragraph className="italic text-zinc-400">First published {format(date)}</Paragraph>
+
+      <details className="mt-10">
         <summary className="font-bold text-white">Notion API response JSON...</summary>
         <Code code={JSON.stringify(post, null, 2)} lang="json" />
       </details>
@@ -45,15 +51,9 @@ function Footer({ post }: FooterProps) {
   )
 
   // return (
-  //   <footer className="mt-12 text-sm text-gray-700 dark:text-gray-300">
-  //     <a href={discussUrl(frontMatter.slug)} target="_blank" rel="noopener noreferrer">
-  //       {'Discuss on Twitter'}
-  //     </a>
-  //     {` • `}
-  //     <a href={editUrl(frontMatter.slug)} target="_blank" rel="noopener noreferrer">
-  //       {'Edit on GitHub'}
-  //     </a>
-  //   </footer>
+  //   <a href={discussUrl(frontMatter.slug)} target="_blank" rel="noreferrer">
+  //     Discuss on Twitter
+  //   </a>
   // )
 }
 
