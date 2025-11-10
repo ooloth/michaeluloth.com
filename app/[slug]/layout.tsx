1e279592c77036c776902a416c3e6eb7f2fa703c
Michael Uloth <hello@michaeluloth.com>
2025-11-09 23:08:37 -0500

    footer: add placeholder text

--- app/[slug]/layout.tsx
+++ app/[slug]/layout.tsx
@@ -1,9 +1,20 @@
 import { type ReactNode } from 'react'
 
+import Footer from '@/ui/footer'
+import Header from '@/ui/header'
+
 type Props = Readonly<{
   children: ReactNode
 }>
 
 export default function PostLayout({ children }: Props) {
-  return <div className="p-4">{children}</div>
+  return (
+    <div className="p-4">
+      <div className="mx-auto max-w-prose">
+        <Header />
+        {children}
+        <Footer />
+      </div>
+    </div>
+  )
 }
