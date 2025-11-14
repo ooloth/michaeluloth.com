f8da5e406e930f39aaaaf4b638ef2863dca599e3
Michael Uloth <hello@michaeluloth.com>
2025-11-13 23:09:54 -0500

    blog: list all published posts

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,5 +1,30 @@
 import { type ReactElement } from 'react'
 
-export default function Blog(): ReactElement {
-  return <div>blog</div>
+import getPosts from '@/lib/notion/getPosts'
+import getPropertyValue from '@/lib/notion/getPropertyValue'
+import Card from '@/ui/card'
+import Link from '@/ui/link'
+
+export default async function Blog(): Promise<ReactElement> {
+  'use cache'
+
+  const posts = await getPosts('descending')
+
+  return (
+    <main className="flex-auto">
+      <h1>Blog</h1>
+      <ul>
+        {posts.map(post => {
+          const title = getPropertyValue(post.properties, 'Title')
+          const slug = getPropertyValue(post.properties, 'Slug')
+
+          return (
+            <li key={post.id} className="flex gap-4">
+              <Card href={`/${slug}/`}>{title}</Card>
+            </li>
+          )
+        })}
+      </ul>
+    </main>
+  )
 }
