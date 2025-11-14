d31c6291218311fa6e85070925ddb3271a5d100a
Michael Uloth <hello@michaeluloth.com>
2025-11-13 23:30:58 -0500

    blog: add type emoji + featured image to cards

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -3,7 +3,9 @@ import { type ReactElement } from 'react'
 import getPosts from '@/lib/notion/getPosts'
 import getPropertyValue from '@/lib/notion/getPropertyValue'
 import Card from '@/ui/card'
-import Link from '@/ui/link'
+import Emoji from '@/ui/emoji'
+import Image from '@/ui/image'
+import Heading from '@/ui/heading'
 
 export default async function Blog(): Promise<ReactElement> {
   'use cache'
@@ -12,15 +14,25 @@ export default async function Blog(): Promise<ReactElement> {
 
   return (
     <main className="flex-auto">
-      <h1>Blog</h1>
-      <ul>
+      <Heading level={1}>Blog</Heading>
+
+      <ul className="mt-6 grid gap-4">
         {posts.map(post => {
           const title = getPropertyValue(post.properties, 'Title')
           const slug = getPropertyValue(post.properties, 'Slug')
+          const type = getPropertyValue(post.properties, 'Type')
+          const image = getPropertyValue(post.properties, 'Featured image')
 
           return (
-            <li key={post.id} className="flex gap-4">
-              <Card href={`/${slug}/`}>{title}</Card>
+            <li key={post.id} className="grid">
+              <Card href={`/${slug}/`}>
+                {image ? <Image url={image} showCaption={false} /> : null}
+
+                <div className="flex items-center gap-2">
+                  <Emoji symbol={type === 'reaction' ? '🔖' : '✍'} />
+                  {title}
+                </div>
+              </Card>
             </li>
           )
         })}
