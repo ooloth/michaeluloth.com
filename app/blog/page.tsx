6ee0a1542411a1c891faceef699d39547790f034
Michael Uloth <hello@michaeluloth.com>
2025-11-13 22:37:10 -0500

    pages: scaffold all missing ones

---
+++ app/blog/page.tsx
@@ -0,0 +1,5 @@
+import { type ReactElement } from 'react'
+
+export default function Blog(): ReactElement {
+  return <div>blog</div>
+}
---
+++ app/videos/page.tsx
@@ -0,0 +1,5 @@
+import { type ReactElement } from 'react'
+
+export default function Videos(): ReactElement {
+  return <div>videos</div>
+}
