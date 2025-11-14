d450bae62626c881ee9b54c20fb649c48c457be3
Michael Uloth <hello@michaeluloth.com>
2025-11-13 22:19:19 -0500

    cloudinary: find public ID of "fetch" images too

--- lib/cloudinary/parsePublicIdFromCloudinaryUrl.ts
+++ lib/cloudinary/parsePublicIdFromCloudinaryUrl.ts
@@ -1,9 +1,12 @@
 /**
  * Parses the public ID from a Cloudinary URL.
- * Assumes the image is stored in the "mu/" folder.
+ * Assumes the image is stored in the "mu/" or "fetch/" folder.
  *
  * Example URL: https://res.cloudinary.com/ooloth/image/upload/v1646239412/mu/be-careful-austin-distel.jpg
  * Example public ID: mu/be-careful-austin-distel
+ *
+ * Example URL: https://res.cloudinary.com/ooloth/image/fetch/https://media.giphy.com/media/l0HlGPNqhq07VzPeU/giphy.gif
+ * Example public ID: https://media.giphy.com/media/l0HlGPNqhq07VzPeU/giphy
  *
  * @throws {TypeError} If the input is not a string.
  * @throws {Error} If the URL is not a valid Cloudinary URL or is not in the "mu/" folder.
@@ -17,27 +20,35 @@ export default function parsePublicIdFromCloudinaryUrl(url: string): string | nu
     throw new Error(`URL must be a Cloudinary URL, but was: ${url}`)
   }
 
-  if (!url.includes('/mu/')) {
-    throw new Error(`Cloudinary URL image must be in the "mu/" folder, but was: ${url}`)
+  if (!url.includes('/mu/') && !url.includes('/fetch/')) {
+    throw new Error(`Cloudinary URL image must be in the "mu/" or "fetch/" folders, but was: ${url}`)
   }
 
-  // Assume public ID will always starts with "mu/"
-  const publicIdStart = url.indexOf('/mu/')
+  const muPublicIdStart = url.indexOf('/mu/')
+  const fetchPublicIdStart = url.indexOf('/fetch/')
+
+  if (muPublicIdStart === -1 && fetchPublicIdStart === -1) {
+    throw new Error(`Failed to parse Public ID from Cloudinary URL: ${url}`)
+  }
+
+  const publicIdStart =
+    muPublicIdStart !== -1
+      ? muPublicIdStart + 1 // +1 to include "mu/"
+      : fetchPublicIdStart + 7 // +7 to exclude "/fetch/"
+
   if (publicIdStart === -1) {
     return null
   }
 
-  // Extract the substring starting from "mu/"
-  const publicIdAndExtension = url.substring(publicIdStart + 1) // +1 to remove leading slash
+  const publicIdAndExtension = url.substring(publicIdStart)
 
-  // Find the last dot to identify the file extension
-  const lastDotIndex = publicIdAndExtension.lastIndexOf('.')
+  return publicIdAndExtension.startsWith('mu/') ? removeFileExtension(publicIdAndExtension) : publicIdAndExtension
+}
+
+const removeFileExtension = (filename: string): string => {
+  const lastDotIndex = filename.lastIndexOf('.')
   if (lastDotIndex === -1) {
-    return publicIdAndExtension // No extension found
+    return filename // No extension found
   }
-
-  // Remove the file extension
-  const publicId = publicIdAndExtension.substring(0, lastDotIndex)
-
-  return publicId
+  return filename.substring(0, lastDotIndex)
 }
