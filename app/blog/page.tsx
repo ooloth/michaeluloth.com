09378c7ce322c47d9a066a4d8d9f968f302344a7
Michael Uloth <hello@michaeluloth.com>
2025-12-09 22:27:27 -0500

    blog: stick with flat style

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -2,9 +2,6 @@ import { type ReactElement } from 'react'
 
 import getPosts from '@/lib/notion/getPosts'
 import getPropertyValue from '@/lib/notion/getPropertyValue'
-import Card from '@/ui/card'
-import Emoji from '@/ui/emoji'
-import Image from '@/ui/image'
 import Heading from '@/ui/heading'
 import { getHumanReadableDate, getMachineReadableDate } from '@/utils/dates'
 
@@ -22,20 +19,17 @@ export default async function Blog({ searchParams }: Props): Promise<ReactElemen
     <main className="flex-auto">
       <Heading level={1}>Blog</Heading>
 
-      <ul className="mt-6 grid gap-10">
-        {/* <ul className="mt-6 grid md:grid-cols-2 gap-4"> */}
+      <ul className="mt-8 grid gap-8">
         {posts.map(post => {
           const slug = getPropertyValue(post.properties, 'Slug')
           const title = getPropertyValue(post.properties, 'Title')
-          const description = getPropertyValue(post.properties, 'Description')
-          const image = getPropertyValue(post.properties, 'Featured image')
           const date = getPropertyValue(post.properties, 'First published')
 
           return (
             <li key={post.id} className="grid">
               <article>
                 <header>
-                  <time datetime={getMachineReadableDate(date)} class="timestamp block mb-0.5 md:min-w-[7rem]">
+                  <time dateTime={getMachineReadableDate(date)} className="timestamp block mb-0.5 md:min-w-28">
                     {getHumanReadableDate(date)}
                   </time>
 
@@ -43,25 +37,7 @@ export default async function Blog({ searchParams }: Props): Promise<ReactElemen
                     {title || slug}
                   </a>
                 </header>
-
-                {/* <div className="pt-2">{description}</div> */}
               </article>
-
-              {/* <Card href={`/${slug}/`}> */}
-              {/*   {image ? ( */}
-              {/*     <Image */}
-              {/*       url={image} */}
-              {/*       showCaption={false} */}
-              {/*       outerStyles="my-0!" */}
-              {/*       imageStyles="rounded-t-xl rounded-b-none" */}
-              {/*     /> */}
-              {/*   ) : null} */}
-              {/**/}
-              {/*   <div className="py-3 px-4"> */}
-              {/*     <p className="text-xl font-medium md:text-lg text-bright">{title}</p> */}
-              {/*     <p className="font-medium md:text-md">{description}</p> */}
-              {/*   </div> */}
-              {/* </Card> */}
             </li>
           )
         })}
