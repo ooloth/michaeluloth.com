ee71d7e2f9a82bad63b0961e4bec88f8462a5410
Michael Uloth <hello@michaeluloth.com>
2025-12-09 22:08:57 -0500

    blog: try flat text with no reliance on images

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -6,6 +6,7 @@ import Card from '@/ui/card'
 import Emoji from '@/ui/emoji'
 import Image from '@/ui/image'
 import Heading from '@/ui/heading'
+import { getHumanReadableDate, getMachineReadableDate } from '@/utils/dates'
 
 type Props = Readonly<{
   searchParams: Promise<{ [key: string]: string | string[] | undefined }>
@@ -21,30 +22,46 @@ export default async function Blog({ searchParams }: Props): Promise<ReactElemen
     <main className="flex-auto">
       <Heading level={1}>Blog</Heading>
 
-      <ul className="mt-6 grid md:grid-cols-2 gap-4">
+      <ul className="mt-6 grid gap-10">
+        {/* <ul className="mt-6 grid md:grid-cols-2 gap-4"> */}
         {posts.map(post => {
-          const title = getPropertyValue(post.properties, 'Title')
           const slug = getPropertyValue(post.properties, 'Slug')
-          const type = getPropertyValue(post.properties, 'Type')
+          const title = getPropertyValue(post.properties, 'Title')
+          const description = getPropertyValue(post.properties, 'Description')
           const image = getPropertyValue(post.properties, 'Featured image')
+          const date = getPropertyValue(post.properties, 'First published')
 
           return (
             <li key={post.id} className="grid">
-              <Card href={`/${slug}/`}>
-                {image ? (
-                  <Image
-                    url={image}
-                    showCaption={false}
-                    outerStyles="my-0!"
-                    imageStyles="rounded-t-xl rounded-b-none"
-                  />
-                ) : null}
+              <article>
+                <header>
+                  <time datetime={getMachineReadableDate(date)} class="timestamp block mb-0.5 md:min-w-[7rem]">
+                    {getHumanReadableDate(date)}
+                  </time>
 
-                <div className="flex gap-3 py-3 px-4">
-                  <Emoji symbol={type === 'reaction' ? '🔖' : '✍'} className="text-2xl" />
-                  <p className="text-xl font-medium md:text-lg">{title}</p>
-                </div>
-              </Card>
+                  <a href={`/${slug}/`} className="link-heading heading text-xl">
+                    {title || slug}
+                  </a>
+                </header>
+
+                {/* <div className="pt-2">{description}</div> */}
+              </article>
+
+              {/* <Card href={`/${slug}/`}> */}
+              {/*   {image ? ( */}
+              {/*     <Image */}
+              {/*       url={image} */}
+              {/*       showCaption={false} */}
+              {/*       outerStyles="my-0!" */}
+              {/*       imageStyles="rounded-t-xl rounded-b-none" */}
+              {/*     /> */}
+              {/*   ) : null} */}
+              {/**/}
+              {/*   <div className="py-3 px-4"> */}
+              {/*     <p className="text-xl font-medium md:text-lg text-bright">{title}</p> */}
+              {/*     <p className="font-medium md:text-md">{description}</p> */}
+              {/*   </div> */}
+              {/* </Card> */}
             </li>
           )
         })}
