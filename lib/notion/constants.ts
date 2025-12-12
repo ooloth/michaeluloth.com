ca3d34d1c04eb7f59d531def5f4ea495ddcd7641
Michael Uloth <hello@michaeluloth.com>
2025-12-11 21:59:33 -0500

    notion: delete unused constants file

--- lib/notion/constants.ts
+++
@@ -1,6 +0,0 @@
-import { env } from '@/lib/env'
-
-export const albumsDbId = process.env.NOTION_DB_ID_ALBUMS
-export const booksDbId = process.env.NOTION_DB_ID_BOOKS
-export const podcastsDbId = process.env.NOTION_DB_ID_PODCASTS
-export const writingDataSourceId = env.NOTION_DATA_SOURCE_ID_WRITING
