c0ebe55c09bc85e406e26bee59b556906fc1ed26
Michael Uloth <hello@michaeluloth.com>
2025-12-14 21:06:18 -0500

    home: add new page design

--- app/(prose)/page.tsx
+++ app/(prose)/page.tsx
@@ -1,3 +1,59 @@
-export default function Home() {
-  return <main className="flex-auto">home</main>
+import { type ReactElement } from 'react'
+
+import Image from '@/ui/image'
+import Paragraph from '@/ui/typography/paragraph'
+import PostList from '@/ui/post-list'
+
+function Summary(): ReactElement {
+  return (
+    <section className="md:flex md:justify-between md:items-end md:gap-x-8">
+      <div>
+        <h2 className="leading-tight text-[2.5rem] font-bold text-bright">Hey, I&apos;m Michael</h2>
+        <Paragraph className="text-lg text-zinc-300">
+          I write code for a living. And for fun. It's hard to stop. Have you tried Claude Code? You really should.
+        </Paragraph>
+        <Paragraph className="text-zinc-300">
+          I've built dozens of polished UIs. And a resilient shopping cart. And highly-validated data pipelines. I care
+          about reliability, ergonomics, and all the tiny details that help projects succeed. I think about what could
+          go wrong and then build systems to ensure that won't happen.
+        </Paragraph>
+      </div>
+
+      <Image
+        url="https://res.cloudinary.com/ooloth/image/upload/v1645057009/mu/michael-landscape.jpg"
+        effect="grayscale"
+        imageStyles="object-cover object-[57%_0%] rounded-3xl! w-48 md:w-56 aspect-square"
+        outerStyles="flex-none md:my-0" // don't shrink
+      />
+    </section>
+  )
+}
+
+type RecentWritingProps = Readonly<{
+  skipCache: boolean
+}>
+
+function RecentWriting({ skipCache }: RecentWritingProps): ReactElement {
+  return (
+    <section>
+      <h2 className="mt-16 mb-4 leading-tight text-[1.65rem] font-semibold text-bright">Recent Writing</h2>
+      <PostList limit={5} skipCache={skipCache} />
+    </section>
+  )
+}
+
+type Props = Readonly<{
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>
+}>
+
+export default async function Home({ searchParams }: Props): Promise<ReactElement> {
+  const params = await searchParams
+  const skipCache = params.nocache === 'true'
+
+  return (
+    <main className="flex-auto">
+      <Summary />
+      <RecentWriting skipCache={skipCache} />
+    </main>
+  )
 }
