0cb5541b4c23d44678ed711f68fb16fc4dca1cd4
Michael Uloth <hello@michaeluloth.com>
2025-12-14 21:05:40 -0500

    footer: restore original look + behaviour

--- ui/footer.tsx
+++ ui/footer.tsx
@@ -1,10 +1,10 @@
-import SecondaryNav from '@/ui/nav/secondary'
 import SocialNav from '@/ui/nav/socials'
+import Paragraph from '@/ui/typography/paragraph'
 
 export default function Footer() {
   return (
-    <footer className="flex flex-col sm:flex-row items-center justify-between gap-2 mt-24 pb-6">
-      <SecondaryNav />
+    <footer className="flex flex-col sm:flex-row items-center justify-between gap-2 mt-24 pb-6 text-zinc-300">
+      <Paragraph className="my-0">&copy; {new Date().getFullYear()} Michael Uloth</Paragraph>
       <SocialNav />
     </footer>
   )
