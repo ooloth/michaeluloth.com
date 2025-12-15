6040ac892f9e1ec2ea9959cd40bf0f15301e65e1
Michael Uloth <hello@michaeluloth.com>
2025-12-14 21:04:53 -0500

    header: restore original look + behaviour

--- ui/header.tsx
+++ ui/header.tsx
@@ -1,16 +1,12 @@
-import Image from '@/ui/image'
-
+import getPosts from '@/io/notion/getPosts'
 import PrimaryNav from '@/ui/nav/primary'
 
-export default function Header() {
+export default async function Header() {
+  const posts = (await getPosts({ sortDirection: 'descending' })).unwrap()
+
   return (
-    <header className="flex items-center justify-between mb-8 pt-4">
-      <PrimaryNav />
-      <Image
-        url="https://res.cloudinary.com/ooloth/image/upload/v1645057009/mu/michael-landscape.jpg"
-        imageStyles="object-cover object-[56%_0%] rounded-full w-[2.2em] h-[2.2em]"
-        outerStyles="mt-0 mb-0" // override default margins
-      />
+    <header className="flex items-center justify-between mb-12 pt-7">
+      <PrimaryNav posts={posts} />
     </header>
   )
 }
