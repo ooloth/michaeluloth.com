95525f5794982be91c3ff59c62937e6ac100670d
Michael Uloth <hello@michaeluloth.com>
2025-12-16 00:26:55 -0500

    posts: remove rss subscription nudge

--- app/(prose)/[slug]/ui/post.tsx
+++ app/(prose)/[slug]/ui/post.tsx
@@ -1,11 +1,9 @@
 import NotionBlocks from '@/io/notion/ui/NotionBlocks'
 import type { Post as PostType, PostListItem } from '@/io/notion/schemas/post'
-// import { Code } from '@/ui/code'
 import PaginationLinks from '@/ui/nav/pagination'
 
 import PostHeader from './post-header'
 import PostFooter from './post-footer'
-import Subscribe from './subscribe'
 
 type Props = Readonly<{
   post: PostType
@@ -19,14 +17,7 @@ export default function Post({ post, prevPost, nextPost }: Props) {
       <article>
         <PostHeader title={post.title} datePublished={post.firstPublished} dateUpdated={post.lastEditedTime} />
         <NotionBlocks blocks={post.blocks} />
-
-        {/* <details className="my-10"> */}
-        {/*   <summary className="font-bold text-white">Notion API response JSON...</summary> */}
-        {/*   <Code code={JSON.stringify(post, null, 2)} lang="json" /> */}
-        {/* </details> */}
-
         <PostFooter />
-        <Subscribe />
         <PaginationLinks prevPost={prevPost} nextPost={nextPost} />
       </article>
     </main>
