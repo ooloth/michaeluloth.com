d63962180707ba18b836b67444cf6014a0328fcb
Michael Uloth <hello@michaeluloth.com>
2025-12-20 20:53:28 -0500

    layout: improve base metadata

--- app/layout.test.tsx
+++ app/layout.test.tsx
@@ -6,9 +6,42 @@ import { describe, expect, it } from 'vitest'
 import { metadata } from './layout'
 
 describe('RootLayout metadata', () => {
-  it('includes basic site information', () => {
-    expect(metadata.title).toBe('Michael Uloth')
-    expect(metadata.description).toBe('Software engineer helping scientists discover new medicines at Recursion.')
+  it('includes metadataBase', () => {
+    expect(metadata.metadataBase).toEqual(new URL('https://michaeluloth.com/'))
+  })
+
+  it('includes title with template', () => {
+    expect(metadata.title).toEqual({
+      default: 'Michael Uloth',
+      template: '%s | Michael Uloth',
+    })
+  })
+
+  it('includes description', () => {
+    expect(metadata.description).toBe(
+      'Software engineer helping scientists discover new medicines at Recursion.',
+    )
+  })
+
+  it('includes authors and creator', () => {
+    expect(metadata.authors).toEqual([{ name: 'Michael Uloth' }])
+    expect(metadata.creator).toBe('Michael Uloth')
+  })
+
+  it('includes OpenGraph config', () => {
+    expect(metadata.openGraph).toEqual({
+      type: 'website',
+      locale: 'en_CA',
+      siteName: 'Michael Uloth',
+      images: ['/og-image.png'],
+    })
+  })
+
+  it('includes Twitter card config', () => {
+    expect(metadata.twitter).toEqual({
+      card: 'summary_large_image',
+      creator: '@ooloth',
+    })
   })
 
   it('includes RSS feed link', () => {
