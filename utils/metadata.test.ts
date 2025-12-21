0ec4723a455eb0d30e35d0c156ba83ece2090180
Michael Uloth <hello@michaeluloth.com>
2025-12-20 23:53:32 -0500

    metadata: simplify test cases

--- utils/metadata.test.ts
+++ utils/metadata.test.ts
@@ -8,62 +8,25 @@ import {
 } from './metadata'
 
 describe('metadata constants', () => {
-  describe('SITE_NAME', () => {
-    it('exports site name', () => {
-      expect(SITE_NAME).toBe('Michael Uloth')
-    })
-
-    it('is a non-empty string', () => {
-      expect(typeof SITE_NAME).toBe('string')
-      expect(SITE_NAME.length).toBeGreaterThan(0)
-    })
+  it('exports site name', () => {
+    expect(SITE_NAME).toBe('Michael Uloth')
   })
 
-  describe('SITE_DESCRIPTION', () => {
-    it('exports site description', () => {
-      expect(SITE_DESCRIPTION).toBe(
-        'Software engineer helping scientists discover new medicines at Recursion.',
-      )
-    })
-
-    it('is a non-empty string', () => {
-      expect(typeof SITE_DESCRIPTION).toBe('string')
-      expect(SITE_DESCRIPTION.length).toBeGreaterThan(0)
-    })
+  it('exports site description', () => {
+    expect(SITE_DESCRIPTION).toBe(
+      'Software engineer helping scientists discover new medicines at Recursion.',
+    )
   })
 
-  describe('SITE_AUTHOR', () => {
-    it('exports site author name', () => {
-      expect(SITE_AUTHOR).toBe('Michael Uloth')
-    })
-
-    it('is a non-empty string', () => {
-      expect(typeof SITE_AUTHOR).toBe('string')
-      expect(SITE_AUTHOR.length).toBeGreaterThan(0)
-    })
+  it('exports site author', () => {
+    expect(SITE_AUTHOR).toBe('Michael Uloth')
   })
 
-  describe('DEFAULT_OG_IMAGE', () => {
-    it('exports default OG image path', () => {
-      expect(DEFAULT_OG_IMAGE).toBe('/og-image.png')
-    })
-
-    it('is a valid path string', () => {
-      expect(typeof DEFAULT_OG_IMAGE).toBe('string')
-      expect(DEFAULT_OG_IMAGE).toMatch(/^\//)
-      expect(DEFAULT_OG_IMAGE.length).toBeGreaterThan(0)
-    })
+  it('exports default OG image path', () => {
+    expect(DEFAULT_OG_IMAGE).toBe('/og-image.png')
   })
 
-  describe('SITE_URL', () => {
-    it('exports site URL', () => {
-      expect(SITE_URL).toBe('https://michaeluloth.com/')
-    })
-
-    it('is a valid URL with trailing slash', () => {
-      expect(typeof SITE_URL).toBe('string')
-      expect(SITE_URL).toMatch(/^https:\/\//)
-      expect(SITE_URL).toMatch(/\/$/)
-    })
+  it('exports site URL', () => {
+    expect(SITE_URL).toBe('https://michaeluloth.com/')
   })
 })
