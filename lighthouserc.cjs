54a03f550333322e03263761ad5eaa542fbad25a
Michael Uloth <hello@michaeluloth.com>
2025-12-21 20:12:58 -0500

    lighthouse: configure ci

---
+++ lighthouserc.cjs
@@ -0,0 +1,17 @@
+module.exports = {
+  ci: {
+    collect: {
+      staticDistDir: './out',
+    },
+    assert: {
+      assertions: {
+        'categories:accessibility': ['error', { minScore: 0.9 }],
+        'categories:performance': ['warn', { minScore: 0.8 }],
+        'categories:seo': ['warn', { minScore: 0.9 }],
+      },
+    },
+    upload: {
+      target: 'temporary-public-storage',
+    },
+  },
+}
