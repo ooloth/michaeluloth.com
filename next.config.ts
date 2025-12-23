f074766d6eceba26cdfdd551a6c0689e0b32dc0d
Michael Uloth <hello@michaeluloth.com>
2025-12-22 22:57:35 -0500

    next: don't optimize images

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
   output: 'export',
   images: {
+    unoptimized: true, // Required for static exports - no image optimization server
     remotePatterns: [
       {
         protocol: 'https',
