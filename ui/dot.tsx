018d84182ced773cf4a544d1e497b5230a7a7372
Michael Uloth <hello@michaeluloth.com>
2025-12-23 11:25:38 -0500

    dot: accept color prop

--- ui/dot.tsx
+++ ui/dot.tsx
@@ -1,3 +1,14 @@
-export default function Dot() {
-  return <span className="font-extrabold text-accent">•</span>
+type Props = Readonly<{
+  color?: 'accent' | 'bright' | 'foreground' | 'muted'
+}>
+
+export default function Dot({ color = 'accent' }: Props) {
+  const dotClassFromColor = {
+    accent: 'text-accent',
+    bright: 'text-bright',
+    foreground: '',
+    muted: 'text-zinc-500',
+  } as const
+
+  return <span className={`font-extrabold ${dotClassFromColor[color]}`}>•</span>
 }
