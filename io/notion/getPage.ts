af627cc3a7f9f55c710314b41e63a8cfdc889488
Michael Uloth <hello@michaeluloth.com>
2025-12-26 20:38:32 -0500

    notion: remove unused getPage helper

--- io/notion/getPage.ts
+++
@@ -1,9 +0,0 @@
-import notion from './client'
-
-const getPage = async (pageId: string) => {
-  const response = await notion.pages.retrieve({ page_id: pageId })
-
-  return response
-}
-
-export default getPage
